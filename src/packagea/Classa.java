9c8707ba95af8127abaa27c7a0707a318b11499d
Rohit-cell-art <70696501+Rohit-cell-art@users.noreply.github.com>
2021-03-27 21:44:36 +0530

    Initial commit

---
+++ src/packagea/Classa.java
@@ -0,0 +1,7 @@
+package packagea;
+
+public class Classa {
+    public static void main(String[] args) {
+        System.out.println("PackageA");
+    }
+}
---
+++ src/packageb/Classb.java
@@ -0,0 +1,8 @@
+package packageb;
+import packagea.Classa;
+public class Classb {
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome To Packageb");
+    }
+}
